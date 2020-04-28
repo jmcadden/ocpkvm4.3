7366ea5de315661595831b8a549beb6046a715c0
muimui3 <lfleung@us.ibm.com>
2020-04-28 13:04:49 -0400

    Update create_kvm.sh
    changed the masters and workers ram from 12 gb to 16 gb, hd from 120 gb to 150 gb.

--- roles/create_ocp4_kvms/files/create_kvm.sh
+++ roles/create_ocp4_kvms/files/create_kvm.sh
@@ -2,8 +2,8 @@
 
 for i in master{0..2}
 do 
-  virt-install --name="ocp4-${i}" --vcpus=4 --ram=12288 \
-  --disk path="$1"/ocp4-${i}.qcow2,bus=virtio,size=120 \
+  virt-install --name="ocp4-${i}" --vcpus=4 --ram=16384 \
+  --disk path="$1"/ocp4-${i}.qcow2,bus=virtio,size=150 \
   --os-variant rhel8.0 --network network="$2",model=virtio \
   --boot menu=on --print-xml > ocp4-$i.xml
   virsh define --file ocp4-$i.xml
@@ -11,8 +11,8 @@ done
 
 for i in worker{0..2} bootstrap
 do 
-  virt-install --name="ocp4-${i}" --vcpus=4 --ram=8192 \
-  --disk path="$1"/ocp4-${i}.qcow2,bus=virtio,size=120 \
+  virt-install --name="ocp4-${i}" --vcpus=4 --ram=16384 \
+  --disk path="$1"/ocp4-${i}.qcow2,bus=virtio,size=150 \
   --os-variant rhel8.0 --network network="$2",model=virtio \
   --boot menu=on --print-xml > ocp4-$i.xml
   virsh define --file ocp4-$i.xml
